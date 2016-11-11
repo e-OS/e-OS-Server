08a7963c72610351d8973ea0b452b3179e444342
O.G.olasowski <golasowski.ondra@gmail.com>
2016-11-11 01:27:34 +0100

    Update index.php

--- demo/index.php
+++ demo/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require __DIR__ . '/../source/eOs/Server/RestServer/RestServer.php';
+require __DIR__ . '/../source/eOS/Server/RestServer/RestServer.php';
 require 'TestController.php';
 
 $server = new \eOS\Server\RestServer\RestServer('debug');
